bfbe4ce53aa5c7886526afbc0af6ce49dd7ffa78
Myth06 <rashmi20010621@gmail.com>
2024-09-11 09:27:57 +0530

    CALENDER
    Google calender for view

---
+++ back-end/src/main/java/com/example/demo/assignedTasksAndMembers/AssignedTasksAndMembersController.java
@@ -0,0 +1,14 @@
+package com.example.demo.assignedTasksAndMembers;//package com.example.demo.assignedTasksAndMembers;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/assignedTasks")
+
+@AllArgsConstructor
+public class AssignedTasksAndMembersController {
+
+}
